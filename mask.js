596ac15d56e19003719319f3c75293f05aa6df56
eivinkas <79715793+eivinkas@users.noreply.github.com>
2023-02-12 18:47:57 +0100

    Update mask.js

--- mask.js
+++ mask.js
@@ -1,4 +1,4 @@
-(typeof version === 'undefined') ? (const version = '0.0.7') : (version = '0.0.7');
+const version = '0.0.8'
 
 // In development
 // Description: Creates a mask object for the jsPsych plugin
