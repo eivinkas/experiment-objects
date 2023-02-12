649c11a1b58fae9647454e711c69f168e07aaa7a
eivinkas <79715793+eivinkas@users.noreply.github.com>
2023-02-12 19:02:49 +0100

    Update trials.js

--- trials.js
+++ trials.js
@@ -11,9 +11,6 @@ dotmaskTrial = function (
   mask_obj,
   data_obj,
 ) {
-  
-  console.log('dotmaskTrial loaded: '+version)
-  
   let trial = {
     type: jsPsychPsychophysics,
     stimuli: [focus_obj, fixation_obj, ...item_obj, mask_obj], // These can be referenced using the jsPsych.currentTrial().stimuli array.
